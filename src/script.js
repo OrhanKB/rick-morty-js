5154be7748258fdd4970519fe37122303647f0c6
Orhan <kurtboganorhan@gmail.com>
2024-04-27 23:04:43 +0300

    public index

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const characters = document.querySelector("#characters")
+const locations = document.querySelector("#locations")
+const favorites = document.querySelector("#favorites")
+
+characters.addEventListener("click", e => {
+    document.location.href = "../src/pages/characters.html"
+} 
+);
+
+locations.addEventListener("click", e => {
+    document.location.href = "../src/pages/locations.html"
+})
+
+favorites.addEventListener("click", e => {
+    document.location.href = "../src/pages/favorites.html"
+})
