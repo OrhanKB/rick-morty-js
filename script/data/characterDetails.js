65625101a084e4ae66fdf100fed89f7ba05eecf6
Orhan <kurtboganorhan@gmail.com>
2025-05-02 17:37:12 +0300

    add renderOtherCharacters detail / update otherCharacters function

--- script/data/characterDetails.js
+++ script/data/characterDetails.js
@@ -1,12 +1,38 @@
 import { urlString } from "./state.js";
-
 //LOCALSTORAGE FOR CHARACTER DETAILS PAGE
-function matchedCharacterId(event) {
+async function matchedCharacterId(event) {
     const eventId = Number(event.target.closest(".cart-html").dataset.cartId)
     const charactersData = urlString.data.results;
     const matchedId = charactersData.find(character => character.id === eventId)
-    localStorage.setItem("matchedId", JSON.stringify(matchedId));    
+    localStorage.setItem("matchedId", JSON.stringify(matchedId));
+    
+    const characterUrl = matchedId.location.url;
+    
+    async function otherCharacters() {
+        try {
+            const res = await fetch(characterUrl);
+            const data = await res.json();
+            const characterUrlArray = data.residents;
+
+            const dataPromises = characterUrlArray.map((async url => {
+                const res = await fetch(url);
+                return res.json();
+            }))
+
+            const allData = await Promise.all(dataPromises)
+           
+            return allData
+
+        } catch(err) {
+            console.log("error:", err)
+        }
+    }
+
+    const allData = await otherCharacters();
+    localStorage.setItem("otherCharactersData" ,JSON.stringify(allData));
+    
     window.location.href = "character-details.html"
+     
 }
 
   function characterDetailEventListener () {
@@ -16,42 +42,4 @@ function matchedCharacterId(event) {
 }
 
 characterDetailEventListener();
-//LOCALSTORAGE FOR CHARACTER DETAILS PAGE
-
-const characterLocationUrl = JSON.parse(localStorage.getItem("matchedId"));
-const characterURL = characterLocationUrl.location.url
- 
- async function otherCharacters() {
-    try {   
-        const res = await fetch(characterURL);
-        const data  = await res.json();
-        const characterUrlArray = data.residents
-        return characterUrlArray
-    } catch(err) {
-        console.log("error:", err)
-    }
-}
-
- const otherCharactersData = await otherCharacters();
-
- async function fetchUrlSequantially(otherCharactersData) {
-    const results = [];
-
-    for(const url of otherCharactersData) {
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            results.push(data)
-            
-        } catch(err) {
-            console.log("err:", error)
-        }
-    }
-    return results
-}
-
-const characters = await fetchUrlSequantially(otherCharactersData);
-characters.forEach((character) => {
-    console.log("character:", character)
-})
 
--- script/renderCharacterDetails.js
+++ script/renderCharacterDetails.js
@@ -1,6 +1,8 @@
 import { urlString } from "./data/state.js";
+//import { characters } from "./data/characterDetails.js";
 
 const parseCharacterDetails = JSON.parse(localStorage.getItem("matchedId"));
+
 function renderCharacterDetails() {
     let detailCardHtml;
 
@@ -57,5 +59,62 @@ function renderCharacterDetails() {
 
 }
 
-renderCharacterDetails()
+renderCharacterDetails();
+
+
+
+const parsedOtherCharactersData = JSON.parse(localStorage.getItem("otherCharactersData"));
+
+/* function renderOtherCharacters() {
+    let otherCharactersHtml = ``
+
+    parsedOtherCharactersData.forEach((character) => {
+
+        otherCharactersHtml =  otherCharactersHtml += `
+            <div class="col-sm-4 mb-3<!--d-flex justify-content-center -->">
+                <img class="details-img rounded-circle mx-2" src="${character.image}" alt="">
+                <div class="details">
+                    <p class="details-name" w-100>${character.name}</p>
+                    <p class="details-location">${character.location.name}</p>
+                    <p class="details-species">${character.species}/${character.gender}</p>
+                </div>
+                <a class="d-flex align-items-center" href=""><img class="details-arrow" src="/public/arrow-89-24.png" alt=""> </a>
+            </div>
+        `
+    })
+    document.querySelector(".other-characters-row").innerHTML = otherCharactersHtml;
+}
+
+renderOtherCharacters(); */
+
+function renderOtherCharacters() {
+    let otherCharactersHtml = ``;
+
+    parsedOtherCharactersData.forEach((character) => {
+        console.log("character:", character)
+        otherCharactersHtml += `
+            <div class="col-sm-4 mb-3"> <!-- Parent column (controls width) -->
+                <div class="d-flex align-items-center overflow-hidden text-nowrap p-2 rounded"> <!-- Flex container for content -->
+                    <img class="details-img rounded-circle me-2" src="${character.image}" alt="${character.name}"  object-fit: cover;">
+                    <div class="details flex-grow-1 overflow-hidden"> <!-- Forces text to respect parent width -->
+                        <p class="details-name mb-1 text-truncate">${character.name}</p>
+                        <p class="details-location mb-1 text-truncate">${character.location.name}</p>
+                        <p class="details-species text-truncate">${character.species}/${character.gender}</p>
+                    </div>
+                    <a class="ms-2 d-flex align-items-center" href="#">
+                        <img class="details-arrow" src="/public/arrow-89-24.png" alt="View" width="16">
+                    </a>
+                </div>
+            </div>
+        `;
+    });
+
+    document.querySelector(".other-characters-row").innerHTML = otherCharactersHtml;
+}
+
+renderOtherCharacters();
+
+
+
+
 
